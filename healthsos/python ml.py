6a8393d0ed07e5346ca84a71ec548f99acb2012c
Vikas_Kumar <103962309+VikasKumar2001@users.noreply.github.com>
2022-12-05 19:23:47 +0530

    Add files via upload

---
+++ healthsos/python ml.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+df_meal = pd.read_csv('meal_info.csv')
+df_center=pd.read_csv('fulfilment_center_info.csv')
+df_center=pd.read_csv('train.csv')
